ff922292ac37c12204b0c25f53bb329d197dead8
Guilherme Henrique <guilherme.henriquept@gmail.com>
2025-07-14 15:33:50 -0300

    alterando para ignorar depois da barra

--- pages/api/status/[code].ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { code } = req.query;
-  const statusCode = parseInt(code as string, 10) || 200;
-  res.status(statusCode).json({ code: statusCode });
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div>
       <h1>API Tester</h1>
-      <p>Use /api/status/[code] to get the requested HTTP status code.</p>
+      <p>
+        Use /api/status/[code]/[anything] to get the requested HTTP status code.
+      </p>
     </div>
   );
 }
