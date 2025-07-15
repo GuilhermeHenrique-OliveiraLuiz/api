5a5a2909b6cbc34d41dc572b8272d7cb06c07a6c
Guilherme Henrique <guilherme.henriquept@gmail.com>
2025-07-15 11:00:43 -0300

    change

---
+++ pages/processess/v1/[...params].ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { params } = req.query;
+
+  if (!params || !Array.isArray(params) || params.length === 0) {
+    res.status(400).json({ error: "Code not provided" });
+    return;
+  }
+
+  const lastSegment = params[params.length - 1]; // Pega a última parte da URL
+  const match = lastSegment.match(/^code(\d{3})$/);
+
+  if (!match) {
+    res
+      .status(400)
+      .json({ error: "Invalid code format. Use /api/status/code500" });
+    return;
+  }
+
+  const statusCode = parseInt(match[1], 10);
+  res.status(statusCode).json({ code: statusCode });
+}
