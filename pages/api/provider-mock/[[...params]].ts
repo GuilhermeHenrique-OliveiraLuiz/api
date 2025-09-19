849bff922fb7b4a96254b9d7e9931df3149a6ea4
Guilherme Henrique <56092483+GuilhermeHenrique-OliveiraLuiz@users.noreply.github.com>
2025-09-19 16:11:39 -0300

    change body mock

--- pages/api/provider-mock/[[...params]].ts
+++ pages/api/provider-mock/[[...params]].ts
@@ -4,28 +4,11 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const mockData = {
-    id: uuidv4(),
+    id: "a1a54c6f-c98d-409d-b716-73a074878fb2",
     status: 3,
-    document: {
-      id: uuidv4(),
-      type: "unico.moja.dictionary.br.cnh.v2.Cnh",
-      cpfMatch: false,
-      faceMatch: true,
-      content: {
-        numero: "1234567890",
-        rgNumero: "MG1234567",
-        cpfNumero: "123.456.789-00",
-        nomeCivil: "João da Silva",
-        filiacao: ["Maria da Silva", "José da Silva"],
-        dataNascimento: "1990-01-01",
-        dataHabilitacao: "2010-06-15",
-        dataExpiracao: "2030-06-15",
-        dataEmissao: "2020-06-15",
-        localEmissao: "São Paulo - SP",
-        categoria: "B",
-      },
-      fileUrls: ["https://api-1313.vercel.app/api/download-pdsf"],
-    },
+    unicoId: { "result": "inconclusive" },
+    identityFraudsters: { "result": "inconclusive" },
+    liveness: 1
   };
 
   return res.status(200).json(mockData);
