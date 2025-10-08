99afc8e5b9c424c32bf2077a5305711257086369
Guilherme Henrique <56092483+GuilhermeHenrique-OliveiraLuiz@users.noreply.github.com>
2025-10-08 15:07:51 -0300

    Update [[...params]].ts

--- pages/api/provider-mock/[[...params]].ts
+++ pages/api/provider-mock/[[...params]].ts
@@ -3,13 +3,6 @@ import { ObjectId } from "bson";
 import { v4 as uuidv4 } from "uuid";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const mockData = {
-  "id": "a1a54c6f-c98d-409d-b716-73a074878fb2",
-  "status": 3,
-  "unicoId": { "result": "inconclusive" },
-  "identityFraudsters": { "result": "inconclusive" },
-  "liveness": 1
-}
-
+  const mockData = {"id":"95f30883-95aa-4452-8422-75f6a92676cf","rank":500}
   return res.status(200).json(mockData);
 }
