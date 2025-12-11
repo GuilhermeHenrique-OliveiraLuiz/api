2252a27c9dd9119a2c39c61a9c907eb790764055
Guilherme Henrique <56092483+GuilhermeHenrique-OliveiraLuiz@users.noreply.github.com>
2025-12-11 15:59:40 -0300

    Update [[...params]].ts

--- pages/api/provider-mock/[[...params]].ts
+++ pages/api/provider-mock/[[...params]].ts
@@ -3,6 +3,32 @@ import { ObjectId } from "bson";
 import { v4 as uuidv4 } from "uuid";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const mockData = {"id":"95f30883-95aa-4452-8422-75f6a92676cf","rank":500}
+  const mockData = {
+    id: new ObjectId(),
+    id: uuidv4(),
+    status: 3,
+    document: {
+      id: new ObjectId(),
+      id: uuidv4(),
+      type: "unico.moja.dictionary.br.cnh.v2.Cnh",
+      cpfMatch: false,
+      faceMatch: true,
+      content: {
+        numero: "1234567890",
+        rgNumero: "MG1234567",
+        cpfNumero: "123.456.789-00",
+        nomeCivil: "João da Silva",
+        filiacao: ["Maria da Silva", "José da Silva"],
+        dataNascimento: "1990-01-01",
+        dataHabilitacao: "2010-06-15",
+        dataExpiracao: "2030-06-15",
+        dataEmissao: "2020-06-15",
+        localEmissao: "São Paulo - SP",
+        categoria: "B",
+      },
+      fileUrls: ["https://api-1313.vercel.app/api/download-pdf"],
+    },
+  };
+
   return res.status(200).json(mockData);
 }
