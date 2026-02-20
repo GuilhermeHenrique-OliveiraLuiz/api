760f6bedb0b5d4703f4aec3f3b4d8e8a95dec1eb
Guilherme Henrique <56092483+GuilhermeHenrique-OliveiraLuiz@users.noreply.github.com>
2026-02-20 16:22:14 -0300

    Update [[...params]].ts
    unicomock

--- pages/api/provider-mock/[[...params]].ts
+++ pages/api/provider-mock/[[...params]].ts
@@ -3,30 +3,7 @@ import { ObjectId } from "bson";
 import { v4 as uuidv4 } from "uuid";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const mockData = {
-    id: uuidv4(),
-    status: 3,
-    document: {
-      id: uuidv4(),
-      type: "unico.moja.dictionary.br.cnh.v2.Cnh",
-      cpfMatch: false,
-      faceMatch: true,
-      content: {
-        numero: "1234567890",
-        rgNumero: "MG1234567",
-        cpfNumero: "123.456.789-00",
-        nomeCivil: "João da Silva",
-        filiacao: ["Maria da Silva", "José da Silva"],
-        dataNascimento: "1990-01-01",
-        dataHabilitacao: "2010-06-15",
-        dataExpiracao: "2030-06-15",
-        dataEmissao: "2020-06-15",
-        localEmissao: "São Paulo - SP",
-        categoria: "B",
-      },
-      fileUrls: ["https://api-1313.vercel.app/api/download-pdf"],
-    },
-  };
+  const mockData = {"id":"7e1656c2-e015-4aca-991c-cf21b77b5092","status":3,"biometryToken":{"result":true}};
 
   return res.status(200).json(mockData);
 }
